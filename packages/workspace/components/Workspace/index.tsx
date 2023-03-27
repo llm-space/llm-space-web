5aa17a8bdd7493bb651ebcffc1604b8ec7b23d11
Henry Li <henry1943@163.com>
2023-03-27 23:49:49 +0800

    feat: integrated with ChatSpace

--- packages/workspace/components/Workspace/index.tsx
+++ packages/workspace/components/Workspace/index.tsx
@@ -1,5 +1,7 @@
 import cn from 'classnames';
 
+import { App as ChatSpace } from '@/chat-space/app';
+
 import styles from './index.module.less';
 
 export interface WorkspaceProps {
@@ -12,7 +14,9 @@ export function Workspace({ className }: WorkspaceProps) {
       <aside className={styles.sideBar}></aside>
       <main className={styles.main}>
         <header className={styles.header}></header>
-        <main className={styles.content}></main>
+        <main className={styles.content}>
+          <ChatSpace />
+        </main>
       </main>
     </div>
   );
