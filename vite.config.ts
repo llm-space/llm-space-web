9f13a34608d13cf31d4adffec5dfeac7c4cd5882
Henry Li <henry1943@163.com>
2023-03-27 11:53:38 +0800

    chore: remove 'replace' plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import replace from '@rollup/plugin-replace';
+// import replace from '@rollup/plugin-replace';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 // import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
@@ -8,18 +8,11 @@ export default defineConfig(() => {
   return {
     plugins: [
       tsconfigPaths(),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-      }),
+      // replace({
+      //   'process.env.NODE_ENV': JSON.stringify('production'),
+      // }),
       // cssInjectedByJsPlugin,
       react({ jsxRuntime: 'classic' }),
     ],
-    // build: {
-    //  lib: {
-    //    entry: resolve(__dirname, 'src/index.tsx'),
-    //	  name: 'MyLib',
-    //	  fileName: 'index',
-    //  },
-    // },
   };
 });
