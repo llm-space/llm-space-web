f4c9398e070d6855a924b5dc94b5464a0c7acae6
Henry Li <henry1943@163.com>
2023-03-29 15:44:53 +0800

    feat: implement ChatManager

--- packages/core/chatting/ChatManager.ts
+++ packages/core/chatting/ChatManager.ts
@@ -1,1 +1,20 @@
-export class ChatManager {}
+import type { Chat } from '../types';
+
+import type { ChatProvider } from './ChatProvider';
+
+export class ChatManager {
+  readonly providers = new Map<string, ChatProvider>();
+
+  async newChat(provider: string): Promise<Chat> {
+    const chatProvider = this.providers.get(provider);
+    if (!chatProvider) {
+      throw new Error(`No chat provider registered with id ${provider}`);
+    }
+    const chat = await chatProvider.newChat();
+    return chat;
+  }
+
+  registerChatProvider(provider: ChatProvider) {
+    this.providers.set(provider.id, provider);
+  }
+}
