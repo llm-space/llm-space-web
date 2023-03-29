d0bcf6f828d5e772af01cd5981f281a7c32415e1
Henry Li <henry1943@163.com>
2023-03-29 15:42:15 +0800

    feat: implement GPTChatProvider

--- packages/core/chatting/gpt/GPTChatProvider.ts
+++ packages/core/chatting/gpt/GPTChatProvider.ts
@@ -1,18 +1,38 @@
-import EventEmitter from 'eventemitter3';
+import { v4 as uuid } from 'uuid';
 
 import type { Message } from '@/core/types';
 
-export interface GPTChatProviderEvents {
-  message: (message: Message) => void;
-  partialMessage: (message: Message) => void;
-}
+import { AbstractChatProvider } from '../AbstractChatProvider';
 
-export class GPTChatProvider extends EventEmitter<GPTChatProviderEvents> {
+const BASE_API_URL = '//localhost:3000/api';
+
+export class GPTChatProvider extends AbstractChatProvider {
   constructor() {
-    super();
+    super('gpt-3.5-turbo', 'GPT-3.5 Turbo');
   }
 
-  async createNew() {}
-
-  async sendMessage(message: Message, history: Message[]) {}
+  async sendMessage(message: Message, history: Message[] = []) {
+    this.emit('message', message);
+    const messages = [...history, message];
+    const res = await fetch(`${BASE_API_URL}/openai/chat/completion`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        messages,
+      }),
+    });
+    const content = await res.text();
+    const responseMessage: Message = {
+      id: uuid(),
+      chatId: message.chatId,
+      sender: {
+        role: 'assistant',
+      },
+      contentType: 'text/markdown',
+      content,
+    };
+    this.emit('message', responseMessage);
+  }
 }
