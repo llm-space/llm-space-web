86e5f17e454a299e6c69d7d3fbfc7e9d60f463c4
Henry Li <henry1943@163.com>
2023-03-30 12:02:09 +0800

    feat: implement MainMenu

--- packages/workbench/components/MainMenu/index.tsx
+++ packages/workbench/components/MainMenu/index.tsx
@@ -1,3 +1,5 @@
+import { MessageOutlined, SoundOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
 import cn from 'classnames';
 
 import styles from './index.module.less';
@@ -7,5 +9,18 @@ export interface MainMenuProps {
 }
 
 export function MainMenu({ className }: MainMenuProps) {
-  return <div className={cn(styles.container, className)}></div>;
+  return (
+    <menu className={cn(styles.container, className)}>
+      <Tooltip title="ChatSpace" placement="right">
+        <li>
+          <MessageOutlined className={cn(styles.icon, styles.active)} />
+        </li>
+      </Tooltip>
+      <Tooltip title="TalkSpace" placement="right">
+        <li>
+          <SoundOutlined className={styles.icon} />
+        </li>
+      </Tooltip>
+    </menu>
+  );
 }
